6b6f6f2bd4fc50a4e89b36526a0296671e2f65ac
lmeniconi <luciano.meniconi.r@gmail.com>
2022-10-11 23:07:45 -0300

    telegram active logic

--- start/telegramBot.ts
+++ start/telegramBot.ts
@@ -8,70 +8,63 @@
 |
 */
 
+import Env from '@ioc:Adonis/Core/Env'
 import { useBot } from 'App/Utils/telegraf'
 
 import User from 'App/Models/User'
 import TelegramChat from 'App/Models/TelegramChat'
 
-const bot = useBot()
+if (Env.get('TELEGRAM_BOT_ACTIVE')) {
+  const bot = useBot()
 
-// Asked Logic memory (use redis probably)
-const wasAsked = {}
+  // Asked Logic memory (use redis probably)
+  const wasAsked = {}
 
-bot.start((ctx) => {
-  ctx.reply('Hola, soy Riporter Bot!')
-  ctx.reply('Para comenzar a usar el bot, escribe /register y sigue las instrucciones.')
-})
+  bot.start((ctx) => {
+    ctx.reply('Hola, soy Riporter Bot!')
+    ctx.reply('Para comenzar a usar el bot, escribe /register y sigue las instrucciones.')
+  })
 
-bot.command('register', (ctx) => {
-  const chatId = ctx.message.chat.id
-  ctx.reply('Escribe el codigo de verificacion de tu cuenta Riporter:')
-  wasAsked[chatId] = 'register_verification_code'
-})
+  bot.command('register', (ctx) => {
+    const chatId = ctx.message.chat.id
+    ctx.reply('Escribe el codigo de verificacion de tu cuenta Riporter:')
+    wasAsked[chatId] = 'register_verification_code'
+  })
 
-bot.on('message', async (ctx) => {
-  const chatId = ctx.message.chat.id
+  bot.on('message', async (ctx) => {
+    const chatId = ctx.message.chat.id
 
-  if (wasAsked[chatId]) {
-    // @ts-ignore
-    const answer = ctx.message.text
+    if (wasAsked[chatId]) {
+      // @ts-ignore
+      const answer = ctx.message.text
 
-    if (wasAsked[chatId] === 'register_verification_code') {
-      const user = await User.findBy('telegramVerificationCode', answer)
-      if (!user) {
-        ctx.reply('El codigo de verificacion es incorrecto, intenta de nuevo.')
-        return
+      if (wasAsked[chatId] === 'register_verification_code') {
+        const user = await User.findBy('telegramVerificationCode', answer)
+        if (!user) {
+          ctx.reply('El codigo de verificacion es incorrecto, intenta de nuevo.')
+          return
+        }
+
+        const telegramChat = new TelegramChat()
+        telegramChat.telegramChatId = chatId
+        telegramChat.userId = user.id
+        await telegramChat.save()
+
+        user.telegramConnected = true
+        user.telegramNotifications = true
+        user.telegramVerificationCode = null
+        await user.save()
+
+        ctx.reply('Tu cuenta ha sido vinculada con exito!')
       }
 
-      const telegramChat = new TelegramChat()
-      telegramChat.telegramChatId = chatId
-      telegramChat.userId = user.id
-      await telegramChat.save()
-
-      user.telegramConnected = true
-      user.telegramNotifications = true
-      user.telegramVerificationCode = null
-      await user.save()
-
-      ctx.reply('Tu cuenta ha sido vinculada con exito!')
+      delete wasAsked[chatId]
     }
+  })
 
-    delete wasAsked[chatId]
-  }
-})
+  bot.launch()
 
-// bot.on('message', async (ctx) => {
-//   // pipe url content
-//   await ctx.replyWithPhoto({
-//     url: 'https://picsum.photos/200/300/?random',
-//     filename: 'kitten.jpg',
-//   })
-// })
-
-// bot.telegram.sendMessage(Env.get('TELEGRAM_CHAT_ID'), 'Hello world')
-
-bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+  // Enable graceful stop
+  process.once('SIGINT', () => bot.stop('SIGINT'))
+  process.once('SIGTERM', () => bot.stop('SIGTERM'))
+}
